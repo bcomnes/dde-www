47401eb5b719501c520f700646f52f85c297bd4a
Bret Comnes <bcomnes@gmail.com>
2018-11-09 16:01:48 -0800

    Add download list

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -6,8 +6,20 @@ const Home = () => (
 		<h1>DDE Releases</h1>
 		<h2>Development Releases</h2>
 		<p>The latest release of Dexter Development Environment. </p>
+		<p>Current version: v2.5.9</p>
+		<ul>
+			<li>Windows</li>
+			<li>Linux</li>
+			<li>macOS</li>
+		</ul>
 		<h2>LTS Releases</h2>
 		<p>Long term support releases.  Will remain stable for a longer duration of time.</p>
+		<p>Current version: v2.5.9</p>
+		<ul>
+			<li>Windows</li>
+			<li>Linux</li>
+			<li>macOS</li>
+		</ul>
 	</div>
 );
 
