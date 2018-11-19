710289a555e4294cc96bbf0f51dfad1e6fed6ca4
Bret Comnes <bcomnes@gmail.com>
2018-11-19 14:54:17 -0800

    Update version numbers

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -6,7 +6,7 @@ const Home = () => (
 		<h1>DDE Releases</h1>
 		<h2>Development Releases</h2>
 		<p>The latest release of Dexter Development Environment. </p>
-		<p>Current version: <a href="https://github.com/cfry/dde/releases/tag/v2.4.3">v2.5.9</a></p>
+		<p>Current version: <a href="https://github.com/cfry/dde/releases/tag/v2.4.3">v2.4.3</a></p>
 		<ul>
 			<li><a href="https://github.com/cfry/dde/releases/download/v2.4.3/dexter_dev_env-Setup-2.4.3.exe">Windows</a></li>
 			<li><a href="https://github.com/cfry/dde/releases/download/v2.4.3/dexter_dev_env-2.4.3-x86_64.AppImage">Linux</a></li>
@@ -14,7 +14,7 @@ const Home = () => (
 		</ul>
 		<h2>LTS Releases</h2>
 		<p>Long term support releases.  Will remain stable for a longer duration of time.</p>
-		<p>Current version: <a href="https://github.com/cfry/dde/releases/tag/v2.4.3">v2.5.9</a></p>
+		<p>Current version: <a href="https://github.com/cfry/dde/releases/tag/v2.4.3">v2.4.3</a></p>
 		<ul>
 			<li><a href="https://github.com/cfry/dde/releases/download/v2.4.3/dexter_dev_env-Setup-2.4.3.exe">Windows</a></li>
 			<li><a href="https://github.com/cfry/dde/releases/download/v2.4.3/dexter_dev_env-2.4.3-x86_64.AppImage">Linux</a></li>
